fbb981a2e181821f55e5c6d7d0b45f84bf3bc624
Gage <gage@rubyshore.com>
2023-06-07 14:22:58 -0500

    fixed the missing data on the home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   before_action :set_user
   def home
     set_random_titles
-    set_genre
+    set_attributes
   end
   
   def about
@@ -14,13 +14,15 @@ class PagesController < ApplicationController
     # Retrieve a random title from the database
     @random_titles = []
     @random_titles << Catalog.order("RANDOM()").limit(3).pluck(:title)
-    @converting_back_to_obj = @random_titles.map { |title| Catalog.find_by(title: title) }
+    @convert_title_to_obj = @random_titles.map { |title| Catalog.find_by(title: title) }
   end
-  def set_genre
-    @converting_back_to_obj.each do |title|
+  def set_attributes
+    @convert_title_to_obj.each do |title|
       @genre = title.genre
-      end
+      @episodes = title.episodes.to_i if title.episodes.present?
+    end
   end
+
   
   def set_user
     @user = User.find(session[:user_id]) if session[:user_id]
