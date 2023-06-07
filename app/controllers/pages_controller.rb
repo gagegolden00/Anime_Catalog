46dd42500c9e16ce5e942d1c5dc3bde2cb42e2e3
Gage <gage@rubyshore.com>
2023-06-07 08:47:32 -0500

    added the logic to transfor @random_titles back to objects to be able to use the helper methods for finding attributes to display in views

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
   
   def home
     set_random_titles
+    set_genre
   end
   
   def about
@@ -13,5 +14,11 @@ class PagesController < ApplicationController
     # Retrieve a random title from the database
     @random_titles = []
     @random_titles << Catalog.order("RANDOM()").limit(3).pluck(:title)
+    @converting_back_to_obj = @random_titles.map { |title| Catalog.find_by(title: title) }
+  end
+  def set_genre
+    @converting_back_to_obj.each do |title|
+      @genre = title.genre
+      end
   end
 end
